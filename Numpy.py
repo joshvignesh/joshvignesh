b179a946c9f7cf1ed6b0ce542ddcacaebc1deca1
joshvignesh <47501245+joshvignesh@users.noreply.github.com>
2019-02-18 17:31:33 +0530

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5,6,7]
+y = [1,4,9,16,25,36,49]
+plt.plot(x,y,color="black",linewidth=2)
+plt.xlabel("n")
+plt.ylabel("n**2")
+plt.title("Line graph")
+plt.show()
